35edd6641ce4db97ccdfdadf62505ebd4cad45ff
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2024-12-22 11:08:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -293,7 +293,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     // Вызов встроенного меню "Поделиться"
                     await navigator.share({
-                        title: "PDF файл",
+                        title: "",
                         text: "",
                         files: [file],
                     });
